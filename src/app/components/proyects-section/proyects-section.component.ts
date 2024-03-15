6c007e982009d16398ac23a81d24c9ef7bf5c970
enzoberrutti <enzo.berrutti@gmail.com>
2024-03-15 14:04:11 -0300

    pre-animations

--- src/app/components/proyects-section/proyects-section.component.ts
+++ src/app/components/proyects-section/proyects-section.component.ts
@@ -25,6 +25,28 @@ export class ProyectsSectionComponent {
   ocultarTexto() {
     this.mostrarDescripcion = false;
   }
+  proyectos = [
+  {
+    title: 'Albumize',
+    description: 'La página web es una plataforma de reseñas de álbumes musicales, donde los usuarios pueden dejar comentarios y puntuaciones para cualquier álbum de música.',
+    lengList: ['Angular', 'TypeScript', 'HTML', 'CSS','Spotify API'],
+    imagenUrl: 'assets/images/albumize-nonav-cuad.jpg'
+  },
+  {
+    title: 'Nerflis',
+    description: 'Esta plataforma es un servicio de streaming desarrollado en Java y JSON, que ofrece a los usuarios acceso a un extenso catálogo de películas y series.',
+    lengList: ['Java', 'JSON'],
+    imagenUrl: 'assets/images/albumize-nonav-cuad.jpg'
+  },
+  {
+    title: 'Mi Portfolio',
+    description: 'Portfolio personal creado utilizando tecnologías modernas como Angular, CSS, HTML y TypeScript. Este portfolio es una muestra de mi trabajo y experiencia en el desarrollo web. ',
+    lengList: ['Angular', 'TypeScript', 'HTML', 'CSS'],
+    imagenUrl: 'assets/images/albumize-nonav-cuad.jpg'
+  }
+
+
+];
 
   
   constructor(){}
